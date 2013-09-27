3e91d1ef8672f6b0922c5e597cc2802583ec35cf
xaedes <xaedes@gmail.com>
2013-09-27 09:33:37 -0400

    more sophisticated acceleration based car control

--- src/ottocar_ui/scripts/joystick.py
+++ src/ottocar_ui/scripts/joystick.py
@@ -15,13 +15,13 @@ from std_msgs.msg import Float32
 
 class Accelerator(object):
     """docstring for Accelerator"""
-    def __init__(self, max_acceleration, max_speed):
+    def __init__(self, minimum_speed, maximum_speed):
         super(Accelerator, self).__init__()
         
         self.speed = 0
+        self.minimum_speed = minimum_speed
+        self.maximum_speed = maximum_speed
         self.acceleration = 0
-        self.max_acceleration = max_acceleration
-        self.max_speed = max_speed
 
         self.clock = pygame.time.Clock()
         self.clock.tick()
@@ -30,36 +30,76 @@ class Accelerator(object):
         dtime = self.clock.get_time() / 1000.0  # delta time in seconds
 
         self.speed += dtime * self.acceleration
-        self.speed = min(self.speed, self.max_speed)
+        self.speed = min(max(self.speed,self.minimum_speed),self.maximum_speed)
 
         self.clock.tick()
 
-class Node(object):
-    """docstring for Node"""
+class AccelerationCarControl(Accelerator):
+    """docstring for AccelerationCarControl"""
+    def __init__(self, minimum_speed, maximum_speed, max_forward_acceleration, max_forward_brake, max_reverse_acceleration, max_reverse_brake):
+        super(AccelerationCarControl, self).__init__(minimum_speed, maximum_speed)
 
-    def rescale(self, value,to_min,to_max,from_min=-1,from_max=1):
+        self.max_forward_acceleration = max_forward_acceleration
+        self.max_forward_brake = max_forward_brake
+        self.max_reverse_acceleration = max_reverse_acceleration
+        self.max_reverse_brake = max_reverse_brake
+        
+    def set_axis_value(self, value):
+        if(self.speed < 0):
+            self.acceleration = Node.axis_rescale(value,-self.max_reverse_acceleration,0,0,self.max_reverse_brake)
+        else:
+            self.acceleration = Node.axis_rescale(value,-self.max_forward_brake,0,0,self.max_forward_acceleration)
+
+
+class Node(object):
+    """Handles input from joystick to publish /speed_cmd and /angle_cmd.
+
+    Remarks:
+    Joystick may not be exactly calibrated, thus it may not be possible to reach maximum values calculated from joystick axes.
+    """
+
+    @staticmethod
+    def rescale(value,to_min,to_max,from_min=-1,from_max=1):
         return ((value-from_min) / (from_max-from_min)) * (to_max-to_min) + to_min
 
+    @staticmethod
+    def axis_rescale(value,to_absmax_negative,to_absmin_negative,to_absmin_positive,to_absmax_positive):
+        '''Rescales input from axis.
+
+        Axis input is in [-1,1]. 
+        Negative values are mapped from [-1,0] to [to_absmax_negative,to_absmin_negative].
+        Positive values are mapped from [0,+1] to [to_absmin_positive,to_absmax_positive].
+        '''
+
+        if(value < 0):
+            return Node.rescale(value,to_absmin_negative,to_absmax_negative,from_min = 0,from_max = -1)
+        else:
+            return Node.rescale(value,to_absmin_positive,to_absmax_positive,from_min = 0,from_max = 1)
+
     def angle_cmd(self, value):
-        value = self.rescale(value,270,450)
+        # value = self.rescale(value,270,450)
+        print value
+        value = self.axis_rescale(value,270,375,375,450)
         self.pub_angle.publish(Int16(data=int(value)))
         print 'angle: ', value
 
     def speed_cmd(self, value):
-        value = self.rescale(value,-20,20)      # moderate speed
+        print value
+        value = self.axis_rescale(value,-20,0,10,50)      
+        # value = self.rescale(value,-20,20)      # moderate speed
         # value = self.rescale(value,-127,127)  # maximum speed
         self.pub_speed.publish(Int8(data=int(value)))
         print 'speed: ', value
 
     def accelerate_cmd(self, value):
-        value = self.rescale(value,-self.accel.max_acceleration,self.accel.max_acceleration)
-        self.accel.acceleration = value
-        self.accel.update()
+        self.accelctrl.set_axis_value(value)
+        self.accelctrl.update()
+        print 'speed: ', self.accelctrl.speed
 
-        print self.accel.speed
         
         # self.pub_speed.publish(Int8(data=int(10)))
-        self.pub_speed.publish(Int8(data=int(self.accel.speed)))
+        self.pub_speed.publish(Int8(data=int(self.accelctrl.speed)))
+
 
     def update(self):
         self.accelerate_cmd( -self.joystick.get_axis( 1 ))
@@ -133,7 +173,13 @@ class Node(object):
             self.pub_angle = rospy.Publisher('angle_cmd', Int16)
             self.pub_speed = rospy.Publisher('speed_cmd', Int8)
 
-            self.accel = Accelerator(max_acceleration = 1, max_speed=20)
+            # self.accel = Accelerator(max_acceleration = 1, max_speed=20)
+            self.accelctrl = AccelerationCarControl(    #accelerations given in 1/s
+                minimum_speed=-20, maximum_speed=20,
+                max_forward_acceleration=10, 
+                max_forward_brake=20, 
+                max_reverse_acceleration=10, 
+                max_reverse_brake=20)
             
             self.spin()
 
