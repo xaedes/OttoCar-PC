e1cbaae726a95950498e5f2ebc5b57bfbff8d3da
xaedes <xaedes@gmail.com>
2013-10-10 01:01:44 -0400

    script that cleans up workspace completely, remakes rosserial and builds ros_lib for arduino

---
+++ mkrs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get directory of this file (http://stackoverflow.com/a/246128/798588)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+rm -rf install/ build/ devel/
+
+RSDIR="$( rospack find rosserial_arduino )/.."
+$RSDIR/remake.sh
+
+source install/setup.bash
+
+rm -rf ~/ino/lib/ros_lib
+rosrun rosserial_arduino make_libraries.py /home/viki/ino/lib
