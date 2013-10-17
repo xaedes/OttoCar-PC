545fbed46f0e6b59e3fe027f7fbec2c9af716275
xaedes <xaedes@gmail.com>
2013-10-17 07:56:16 -0400

    use of measure sample rate object from ottocar_utils instead of local class

--- src/ottocar_navigation/scripts/high_pass_accel.py
+++ src/ottocar_navigation/scripts/high_pass_accel.py
@@ -15,6 +15,7 @@ from std_msgs.msg import Int32
 from sensor_msgs.msg import Joy
 
 from ottocar_utils import spark
+from ottocar_utils import MeasureSampleRate
 
 import numpy as np
 
@@ -23,42 +24,7 @@ from numpy.fft import fft,ifft
 from pylab import plot, show, title, xlabel, ylabel, subplot
 # from scipy import fft, arange
 
-class MeasureSampleRate(object):
-    """docstring for MeasureSampleRate"""
-    def __init__(self, update_interval, gain):
-        super(MeasureSampleRate, self).__init__()
-        self.sample_rate = 1
-        self.gain = gain
-        self.update_interval = update_interval #in number of samples
-        self.first_samplerate = False
-        self.n_samples = 0
-        self.last_time = rospy.Time.now().to_sec()
 
-    def add_sample(self):
-        self.n_samples = (self.n_samples + 1) % self.update_interval
-        if self.n_samples == 0:
-            self.update_sample_rate(self.update_interval)
-
-    def update_sample_rate(self, n_new_samples = 1):
-        dtime = rospy.Time.now().to_sec() - self.last_time
-        self.last_time = rospy.Time.now().to_sec()
-
-        current_sample_rate = n_new_samples / dtime
-
-        if self.first_samplerate:
-            self.first_samplerate = False
-            self.sample_rate = current_sample_rate
-
-        self.sample_rate = self.sample_rate * (1-self.gain) + current_sample_rate * self.gain        
-
-    def __complex__(self):
-        return complex(self.sample_rate)
-    def __int__(self):
-        return int(self.sample_rate)
-    def __long__(self):
-        return long(self.sample_rate)
-    def __float__(self):
-        return float(self.sample_rate)
 
 
 class Window(object):
