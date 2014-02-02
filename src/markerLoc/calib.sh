711f8cc157c19baacb35db1d305f2a9fa5a18be1
xaedes <xaedes@gmail.com>
2014-02-02 13:41:02 +0100

    calibration file now uses more precise chess square width

--- src/markerLoc/calib.sh
+++ src/markerLoc/calib.sh
@@ -3,5 +3,5 @@
 # http://wiki.ros.org/camera_calibration/Tutorials/MonocularCalibration
 
 echo Follow instructions at "http://wiki.ros.org/camera_calibration/Tutorials/MonocularCalibration"
-rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.07 image:=/camera/image_raw 
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.071 image:=/camera/image_raw 
 
--- src/markerLoc/camcheck.sh
+++ src/markerLoc/camcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun camera_calibration cameracheck.py --size 8x6 --square 0.07 /monocular:=/camera /camera/image_rect:=/camera/image_raw
+rosrun camera_calibration cameracheck.py --size 8x6 --square 0.071 /monocular:=/camera /camera/image_rect:=/camera/image_raw
