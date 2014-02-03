b555cddf5464d2b295b74630e2939e81799414a2
xaedes <xaedes@gmail.com>
2014-02-03 15:13:33 +0100

    first approach for motor data(revolutions and rps) filtering

--- src/ottocar_navigation/scripts/kalman.py
+++ src/ottocar_navigation/scripts/kalman.py
@@ -2,6 +2,7 @@
 import rospy
 import numpy as np
 from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 from geometry_msgs.msg import Vector3Stamped
 from sensor_msgs.msg import Imu
 
@@ -145,22 +146,66 @@ class ImuSensorsBiasFilter(ImuSensorsFilter):
         # R: Messunsicherheit
         self.R *= 1
 
+class MotorFilter(Kalman):
+    """docstring for ImuSensorsFilter"""
+    def __init__(self, dt=1):
+        super(MotorFilter, self).__init__(n_states = 2, n_sensors = 1)
+
+        self.dt = dt    # dt in [sec]
+
+        #states:
+        #  revolutions:      0
+        #  rps:              1
+
+        #sensors:
+        #  revolutions:      0
+        #  rps:              1
+
+        # H: Messmatrix
+        self.H = np.matrix( '1 0;'    #revolutions
+                            '0 1'     #rps
+                            )
+        # F: Dynamik
+        self.F = np.matrix([[1,dt],    #revolutions = revolutions + rps*dt
+                            [0,1]     #rps = rps
+                            ])
+
+        # Q: Unsicherheit der Dynamik 
+        self.Q = np.matrix(np.identity(self.n_states)) * 0.1    
+             
+        # P: Unsicherheit ueber Systemzustand   
+        self.P *= 0.1
+
+        # R: Messunsicherheit
+        self.R *= 1
+
+    def update_dt(self,dt): # dt in [sec]
+        self.F[0,1] = dt;
+
+    def measure(self,revolutions,rps):
+        Z = np.matrix([revolutions,rps]).getT()
+
+        self.update(Z)
+        self.predict()
+
 class Subscriber(object):
     """docstring for Subscriber"""
     def __init__(self):
         super(Subscriber, self).__init__()
         rospy.init_node('kalman', anonymous=True)
 
-        self.dt = dt = 1
+        self.dt = 1./80. # topic rate ~ 80hz
 
         self.sensors = ImuSensorsFilter()
         self.sensor_biases = ImuSensorsBiasFilter()
+        self.motor = MotorFilter(self.dt)    
 
 
         # Publishers
         self.pub_imu = rospy.Publisher('/imu/data_filtered', Imu)
         self.pub_imu_bias = rospy.Publisher('/imu/data_biases', Imu)
         self.pub_mag = rospy.Publisher('/imu/mag_filtered', Vector3Stamped)
+        self.pub_rps = rospy.Publisher('/rps_filtered', Float32)
 
         # Subscribers
         rospy.Subscriber('/imu/data_raw', Imu, self.callback_imu)
@@ -168,7 +213,7 @@ class Subscriber(object):
         rospy.Subscriber('/sensor_distance', Int32, self.callback_revolutions)
 
         self.imu = self.mag = self.revolutions = None
-        self.last_revolutions = None
+        self.last_revolutions = self.last_time = None
         self.lock = threading.Lock()
 
         rospy.spin()
@@ -194,6 +239,7 @@ class Subscriber(object):
         self.measure()
         self.lock.release()
 
+
     def measure(self):
         # only proceed if we have all msgs
         if((self.mag==None)or(self.imu==None)or(self.revolutions==None)):
@@ -202,11 +248,29 @@ class Subscriber(object):
         if(self.last_revolutions==None):
             self.last_revolutions=self.revolutions
 
+        if(self.last_time==None):
+            self.last_time=self.imu.header.stamp.to_sec()
+        # else:
+            # self.dt=
+
         # update bias if revolutions don't change, i.e. the car is standing still
         if(self.revolutions-self.last_revolutions == 0):
             self.sensor_biases.measure(self.imu,self.mag)
 
+        # update sensors
         self.sensors.measure(self.imu,self.mag,self.sensor_biases.x)
+
+        # update motor
+        if(self.dt!=0):
+            rps = (self.revolutions - self.last_revolutions) / self.dt # that is nasty, extended kalman filter needed...
+        else:
+            rps = self.motor.x[1]
+
+        # print self.motor.x
+        # self.motor.x[1] = rps
+
+        self.motor.measure(self.revolutions,rps)
+
 
         header = self.mag.header
 
@@ -238,8 +302,13 @@ class Subscriber(object):
         mag.vector.z = self.sensors.x[8]
         self.pub_mag.publish(mag)
 
+        self.pub_rps.publish(self.motor.x[1])
+
+        self.last_revolutions=self.revolutions
+        
         # remove old msgs
         self.imu = self.mag = self.revolutions = None
+
 
         
 
