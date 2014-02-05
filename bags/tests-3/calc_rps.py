3b0d0425af8e90946b9e790098ec819a60fcc3ac
xaedes <xaedes@gmail.com>
2014-02-05 20:50:41 +0100

    use micro controller time for dt

--- bags/tests-3/calc_rps.py
+++ bags/tests-3/calc_rps.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import rospy
-from time import time
+# from time import time
 from std_msgs.msg import Int32
+from std_msgs.msg import UInt32
 from std_msgs.msg import Float32
 
 class Subscriber(object):
@@ -12,40 +13,45 @@ class Subscriber(object):
 
         self.gain = 0.25
         self.pub_rps = rospy.Publisher('/sensor_motor_rps', Float32)
-        self.pub_dtime = rospy.Publisher('/dtime', Float32)
-        self.pub_int = rospy.Publisher('/rps_integrate', Float32)
+        # self.pub_dt = rospy.Publisher('/dt', Float32)
+        # self.pub_int = rospy.Publisher('/rps_integrate', Float32)
 
+        self.time = None
         self.last_time = None
+        self.dt = None
+
         self.revolutions_last = None
         self.rps = 0
 
         self.integrated = 0
 
         rospy.Subscriber('/sensor_motor_revolutions', Int32, self.callback_revolutions)
+        rospy.Subscriber('/uC_time', UInt32, self.callback_uCTime)
         rospy.spin()
+
+    def callback_uCTime(self, msg):
+        self.last_time = self.time
+        self.time = msg.data / 1000.
+        if(self.last_time != None):
+            self.dt = self.time - self.last_time
 
     def callback_revolutions(self, msg):
         revolutions = msg.data
 
-        if(self.revolutions_last==None):
+        if(self.revolutions_last == None):
             self.revolutions_last = revolutions
 
-        if(self.last_time == None):
-            self.last_time = time()
-        else:
-            dtime = time() - self.last_time
-            self.last_time = time()
-
-            current_rps = (revolutions - self.revolutions_last)/dtime
+        if((self.dt != None)and(self.dt>0)):
+            current_rps = (revolutions - self.revolutions_last)/self.dt
             if(current_rps > 10000):    #must be wrong
                 current_rps = self.rps
             self.rps = self.gain * current_rps + (1-self.gain) * self.rps
 
-            self.integrated += dtime * self.rps
+            self.integrated += self.dt * self.rps
 
             self.pub_rps.publish(self.rps)
-            self.pub_dtime.publish(dtime)
-            self.pub_int.publish(self.integrated)
+            # self.pub_dt.publish(self.dt)
+            # self.pub_int.publish(self.integrated)
 
         self.revolutions_last = revolutions
 
